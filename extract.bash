9e6cc1902e92216ee6b3ecb2f5cec3f173477a3e
Ulrik Sandberg <ulrik.sandberg@gmail.com>
2018-07-10 13:04:28 +0200

    2018-07-10. Added Sverige-small.

--- extract.bash
+++ extract.bash
@@ -9,6 +9,10 @@ for market in Sverige Norge Danmark; do
     done
 done
 
+# Sverige small
+echo "Fetching Sverige small..."
+curl -s -b "JSESSIONID=$sessionid" 'https://www.nordnet.se/mux/web/marknaden/kurslista/aktier.html?marknad=Sverige&lista=1_1&small=on&sektor=0&subtyp=key_ratios' | hxnormalize -x | hxselect table#kurstabell | asc2xml | ruby ./extract.rb > Sverige-small.csv
+
 # First North
 echo "Fetching Sverige First North..."
 curl -s -b "JSESSIONID=$sessionid" 'https://www.nordnet.se/mux/web/marknaden/kurslista/aktier.html?marknad=Sverige&lista=6_0&subtyp=key_ratios' | hxnormalize -x | hxselect table#kurstabell | asc2xml | ruby ./extract.rb > Sverige-firstnorth.csv
