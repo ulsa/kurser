7491a8b1bed11e8caa390d2bb659ecf4ee82357e
Ulrik Sandberg <ulrik.sandberg@gmail.com>
2018-10-13 13:50:13 +0200

    2018-09-30

--- extract.bash
+++ extract.bash
@@ -7,7 +7,7 @@ which asc2xml > /dev/null || (echo "Error: missing 'asc2xml'; brew install html-
 
 sessionid=$1
 
-for market in Sverige Norge Danmark; do
+for market in Sverige Norge Danmark Finland; do
     for cap in large mid; do
         echo "Fetching $market $cap..."
         curl -s -b "JSESSIONID=$sessionid" "https://www.nordnet.se/mux/web/marknaden/kurslista/aktier.html?marknad=${market}&lista=1_1&${cap}=on&sektor=0&subtyp=key_ratios" | hxnormalize -x | hxselect table#kurstabell | asc2xml | ruby ./extract.rb > ${market}-${cap}.csv
